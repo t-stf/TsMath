5eda662748059fd2ab51d6cffbb5175009b75528
Thomas Steinfeld <>
2018-05-13 16:53:06 +0200

    Integer math completed.

--- TsMath/IntegerMath.cs
+++ TsMath/IntegerMath.cs
@@ -9,6 +9,135 @@ namespace TsMath
 	/// </summary>
 	public static partial class IntegerMath
 	{
+
+		/// <summary>
+		/// Determines whether the given number is even.
+		/// </summary>
+		/// <param name="num">The number to check.</param>
+		/// <returns>
+		///   <b>true</b> if the specified number is even; otherwise, <b>false</b>.
+		/// </returns>
+		public static bool IsEven(this int num)
+		{
+			return (num & 1) == 0;
+		}
+
+		/// <summary>
+		/// Determines whether the given number is even.
+		/// </summary>
+		/// <param name="num">The number to check.</param>
+		/// <returns>
+		///   <b>true</b> if the specified number is even; otherwise, <b>false</b>.
+		/// </returns>
+		public static bool IsEven(this long num)
+		{
+			return (num & 1) == 0;
+		}
+
+		/// <summary>
+		/// Calculates the factorial (<paramref name="n"/>!) of the given number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>the factorial (<paramref name="n"/>!)</returns>
+		/// <exception cref="ArgumentOutOfRangeException">If <paramref name="n"/> is negative.</exception>
+		public static BigInteger Factorial(this int n)
+		{
+			if (n < 0)
+				throw new ArgumentOutOfRangeException("n>0 expected");
+			if (n < 2)
+				return 1;
+			BigInteger prod = BigInteger.One;
+			for (int i = 2; i <= n; i++)
+			{
+				prod *= i;
+			}
+			return prod;
+		}
+
+		/// <summary>
+		/// Calculates the Legendre symbol. See https://en.wikipedia.org/wiki/Legendre_symbol for a definition.
+		/// </summary>
+		/// <remarks>
+		/// <para>
+		/// The algorithm is not guaranteed to terminate if <paramref name="p"/> is not prime.
+		/// </para>
+		/// </remarks>
+		/// <param name="a">Number</param>
+		/// <param name="p">Prime number.</param>
+		/// <returns>-1, 0 or 1.</returns>
+		public static BigInteger LegendreSymbol(BigInteger a, BigInteger p)
+		{
+			var p1 = p - 1;
+			var b = p1 >> 1;
+			var ls = PowMod(a, b, p);
+			if (p1 == ls)
+				return -1;
+			if (ls == BigInteger.One)
+				return 1;
+			return 0;
+		}		
+
+		/// <summary>
+		/// Find a quadratic residue (mod p) of 'a'. p	must be a prime.	 
+		/// </summary>
+		/// <remarks>
+		/// This function calculates the values of x in the equation x^2 = a (mod p). It uses the Tonelli-Shanks algorithm.
+		/// The result could be considered as square root of <paramref name="a"/> modulo <paramref name="p"/>.
+		/// <para>
+		/// The algorithm is not guaranteed to terminate if <paramref name="p"/> is not prime.
+		/// </para>
+		/// </remarks>
+		/// <param name="a">First number.</param>
+		/// <param name="p">Modulo number (must be a prime).</param>
+		/// <returns>The quadratic residue or  <see cref="BigInteger.Zero"/>, if it does not exist.</returns>
+		public static BigInteger QuadraticResidue(BigInteger a, BigInteger p)
+		{
+			if (a.Sign < 0 || p.Sign <= 0)
+				throw new ArgumentException("Arguments must be positive.");
+			var zero = BigInteger.Zero;
+			if (a.IsZero || LegendreSymbol(a, p) != 1)
+				return zero;
+			var two = BigInteger.Two;
+			if (p == two)
+				return a;
+			var one = BigInteger.One;
+			var q = p - one;
+			int s = 0;
+			while (q.IsEven)
+			{
+				q >>= 1;
+				s++;
+			}
+			var z = two;
+			while (LegendreSymbol(z, p) != -1)
+				z++;
+			var c = BigInteger.Pow(z, q);
+			var r = PowMod(a, (q + one) >> 1, p);
+			var t = PowMod(a, q, p);
+			var m = s;
+			while (true)
+			{
+				if (t == one)
+					return r;
+				int i = 0;
+				var ts = t;
+				while (true)
+				{
+					var mod = PowMod(t, one << i, p);
+					if (mod == one)
+						break;
+					i++;
+				}
+				var b = PowMod(c, one << (m - i - 1), p);
+				r = (r * b) % p;
+				var b2 = b * b;
+				t = (t * b2) % p;
+				c = b2;
+				m = i;
+			}
+
+		}
+
 	}
 
 }
--- TsMath/IntegerMath.gen.cs
+++ TsMath/IntegerMath.gen.cs
@@ -26,6 +26,106 @@ namespace TsMath
 				a = b; b = r;
 			}
 		}
+
+		/// <summary>
+		/// Computes the least common multiple.
+		/// </summary>
+		/// <param name="a">First number.</param>
+		/// <param name="b">Second number.</param>
+		/// <returns>The least common multiple.</returns>
+		public static int Lcm(int a, int b)
+		{
+			var gcd = Gcd(a, b);
+			return (a / gcd) * b;
+		}
+
+		/// <summary>
+		/// Computes the module of the power (a^exp) % mod.
+		/// </summary>
+		/// <param name="a">Base number.</param>
+		/// <param name="exp">Exponent.</param>
+		/// <param name="mod">Modulus.</param>
+		/// <exception cref="ArithmeticException">The exponent is negative or the modulus is not positive.</exception>
+		/// <returns>(a^exp) % mod.</returns>
+		public static int PowMod(int a, int exp, int mod)
+		{
+			if (exp < 0)
+				throw new ArithmeticException("PowMod: negative exponent not allowed.");
+			if (mod <= 0)
+				throw new ArithmeticException("Modulus must be positive");
+			if (exp == 0)
+				return 1;
+			var x = a;
+			var m = exp;
+			int temp = 1;
+			while (m > 0)
+			{
+				while ((m & 1) == 0)
+				{
+					x = (x * x) % mod;
+					m >>= 1;
+				}
+				temp = (temp * x) % mod;
+				m--;
+			}
+			return temp;
+		}
+
+		/// <summary>
+		/// Calculates the extended Euclidean Algorithm: gcd(a,b)= ax+by. 
+		/// Expects a>b 
+		/// </summary>
+		/// <param name="a">1. number</param>
+		/// <param name="b">2. number</param>
+		/// <param name="x">1. factor</param>
+		/// <param name="y">2. factor</param>
+		/// <returns>GCD of a and b</returns>
+		public static int ExtendedGcd(int a, int b, out int x, out int y)
+		{
+			int r0 = a, r1 = b;
+			int zero = 0;
+			int s0 = 1, s1 = zero;
+			int t0 = s1, t1 = s0;
+			int i = 1;
+			while (!r1.Equals(zero))
+			{
+				var q = r0 / r1;
+				var r = r0 - q * r1;
+				var s = s0 - q * s1;
+				var t = t0 - q * t1;
+				i++;
+				r0 = r1; r1 = r; t0 = t1; t1 = t;
+				s0 = s1; s1 = s;
+			}
+			x = s0; y = t0;
+			return r0;
+		}
+
+
+		/// <summary>
+		/// Calculates the power of <paramref name="a"/> raised to <paramref name="exp"/>, which must be a non negative 
+		/// natural number
+		/// </summary>
+		/// <param name="a">Number.</param>
+		/// <param name="exp">The exponent.</param>
+		/// <returns><paramref name="a"/>^<paramref name="exp"/></returns>
+		/// <exception cref="System.ArgumentException">exp>=0 expected</exception>
+		public static int Pow(int a, int exp)
+		{
+			if (exp < 0)
+				throw new ArgumentException("exp>=0 expected");
+
+			int prod = 1;
+			int factor = a;
+			while (exp != 0)
+			{
+				if (!exp.IsEven())
+					prod *= factor;
+				factor = factor * factor;
+				exp >>= 1;
+			}
+			return prod;
+		}
 	}
 }
 
@@ -57,6 +157,106 @@ namespace TsMath
 				a = b; b = r;
 			}
 		}
+
+		/// <summary>
+		/// Computes the least common multiple.
+		/// </summary>
+		/// <param name="a">First number.</param>
+		/// <param name="b">Second number.</param>
+		/// <returns>The least common multiple.</returns>
+		public static long Lcm(long a, long b)
+		{
+			var gcd = Gcd(a, b);
+			return (a / gcd) * b;
+		}
+
+		/// <summary>
+		/// Computes the module of the power (a^exp) % mod.
+		/// </summary>
+		/// <param name="a">Base number.</param>
+		/// <param name="exp">Exponent.</param>
+		/// <param name="mod">Modulus.</param>
+		/// <exception cref="ArithmeticException">The exponent is negative or the modulus is not positive.</exception>
+		/// <returns>(a^exp) % mod.</returns>
+		public static long PowMod(long a, long exp, long mod)
+		{
+			if (exp < 0)
+				throw new ArithmeticException("PowMod: negative exponent not allowed.");
+			if (mod <= 0)
+				throw new ArithmeticException("Modulus must be positive");
+			if (exp == 0)
+				return 1;
+			var x = a;
+			var m = exp;
+			long temp = 1;
+			while (m > 0)
+			{
+				while ((m & 1) == 0)
+				{
+					x = (x * x) % mod;
+					m >>= 1;
+				}
+				temp = (temp * x) % mod;
+				m--;
+			}
+			return temp;
+		}
+
+		/// <summary>
+		/// Calculates the extended Euclidean Algorithm: gcd(a,b)= ax+by. 
+		/// Expects a>b 
+		/// </summary>
+		/// <param name="a">1. number</param>
+		/// <param name="b">2. number</param>
+		/// <param name="x">1. factor</param>
+		/// <param name="y">2. factor</param>
+		/// <returns>GCD of a and b</returns>
+		public static long ExtendedGcd(long a, long b, out long x, out long y)
+		{
+			long r0 = a, r1 = b;
+			long zero = 0;
+			long s0 = 1, s1 = zero;
+			long t0 = s1, t1 = s0;
+			int i = 1;
+			while (!r1.Equals(zero))
+			{
+				var q = r0 / r1;
+				var r = r0 - q * r1;
+				var s = s0 - q * s1;
+				var t = t0 - q * t1;
+				i++;
+				r0 = r1; r1 = r; t0 = t1; t1 = t;
+				s0 = s1; s1 = s;
+			}
+			x = s0; y = t0;
+			return r0;
+		}
+
+
+		/// <summary>
+		/// Calculates the power of <paramref name="a"/> raised to <paramref name="exp"/>, which must be a non negative 
+		/// natural number
+		/// </summary>
+		/// <param name="a">Number.</param>
+		/// <param name="exp">The exponent.</param>
+		/// <returns><paramref name="a"/>^<paramref name="exp"/></returns>
+		/// <exception cref="System.ArgumentException">exp>=0 expected</exception>
+		public static long Pow(long a, int exp)
+		{
+			if (exp < 0)
+				throw new ArgumentException("exp>=0 expected");
+
+			long prod = 1;
+			long factor = a;
+			while (exp != 0)
+			{
+				if (!exp.IsEven())
+					prod *= factor;
+				factor = factor * factor;
+				exp >>= 1;
+			}
+			return prod;
+		}
 	}
 }
 
@@ -87,6 +287,106 @@ namespace TsMath
 				r = a % b;
 				a = b; b = r;
 			}
+		}
+
+		/// <summary>
+		/// Computes the least common multiple.
+		/// </summary>
+		/// <param name="a">First number.</param>
+		/// <param name="b">Second number.</param>
+		/// <returns>The least common multiple.</returns>
+		public static BigInteger Lcm(BigInteger a, BigInteger b)
+		{
+			var gcd = Gcd(a, b);
+			return (a / gcd) * b;
+		}
+
+		/// <summary>
+		/// Computes the module of the power (a^exp) % mod.
+		/// </summary>
+		/// <param name="a">Base number.</param>
+		/// <param name="exp">Exponent.</param>
+		/// <param name="mod">Modulus.</param>
+		/// <exception cref="ArithmeticException">The exponent is negative or the modulus is not positive.</exception>
+		/// <returns>(a^exp) % mod.</returns>
+		public static BigInteger PowMod(BigInteger a, BigInteger exp, BigInteger mod)
+		{
+			if (exp < 0)
+				throw new ArithmeticException("PowMod: negative exponent not allowed.");
+			if (mod <= 0)
+				throw new ArithmeticException("Modulus must be positive");
+			if (exp == 0)
+				return 1;
+			var x = a;
+			var m = exp;
+			BigInteger temp = 1;
+			while (m > 0)
+			{
+				while ((m & 1) == 0)
+				{
+					x = (x * x) % mod;
+					m >>= 1;
+				}
+				temp = (temp * x) % mod;
+				m--;
+			}
+			return temp;
+		}
+
+		/// <summary>
+		/// Calculates the extended Euclidean Algorithm: gcd(a,b)= ax+by. 
+		/// Expects a>b 
+		/// </summary>
+		/// <param name="a">1. number</param>
+		/// <param name="b">2. number</param>
+		/// <param name="x">1. factor</param>
+		/// <param name="y">2. factor</param>
+		/// <returns>GCD of a and b</returns>
+		public static BigInteger ExtendedGcd(BigInteger a, BigInteger b, out BigInteger x, out BigInteger y)
+		{
+			BigInteger r0 = a, r1 = b;
+			BigInteger zero = 0;
+			BigInteger s0 = 1, s1 = zero;
+			BigInteger t0 = s1, t1 = s0;
+			int i = 1;
+			while (!r1.Equals(zero))
+			{
+				var q = r0 / r1;
+				var r = r0 - q * r1;
+				var s = s0 - q * s1;
+				var t = t0 - q * t1;
+				i++;
+				r0 = r1; r1 = r; t0 = t1; t1 = t;
+				s0 = s1; s1 = s;
+			}
+			x = s0; y = t0;
+			return r0;
+		}
+
+
+		/// <summary>
+		/// Calculates the power of <paramref name="a"/> raised to <paramref name="exp"/>, which must be a non negative 
+		/// natural number
+		/// </summary>
+		/// <param name="a">Number.</param>
+		/// <param name="exp">The exponent.</param>
+		/// <returns><paramref name="a"/>^<paramref name="exp"/></returns>
+		/// <exception cref="System.ArgumentException">exp>=0 expected</exception>
+		public static BigInteger Pow(BigInteger a, int exp)
+		{
+			if (exp < 0)
+				throw new ArgumentException("exp>=0 expected");
+
+			BigInteger prod = 1;
+			BigInteger factor = a;
+			while (exp != 0)
+			{
+				if (!exp.IsEven())
+					prod *= factor;
+				factor = factor * factor;
+				exp >>= 1;
+			}
+			return prod;
 		}
 	}
 }
