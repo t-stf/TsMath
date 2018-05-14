c6ca7ef4bc5be2de39b63a4e9a05aee67daf64ab
Thomas Steinfeld <>
2018-05-14 16:13:59 +0200

    gaussian random numbers

--- TsMath/RandomXorShift.cs
+++ TsMath/RandomXorShift.cs
@@ -13,7 +13,7 @@ namespace TsMath
 
 		ulong s0, s1;
 
-		Func<ulong> nextSampleFunc;
+		internal Func<ulong> nextSampleFunc;
 
 		ulong NextSample()
 		{
@@ -65,7 +65,7 @@ namespace TsMath
 		/// Providing an identical <paramref name="seed"/> value to different <see cref="RandomXorShift"/> objects 
 		/// causes each instance to produce an identical sequences of random numbers. 
 		/// </remarks>
-		public RandomXorShift(int seed, bool threadSafe = false) : this((ulong)seed, 0, threadSafe) { }
+		public RandomXorShift(int seed, bool threadSafe = false) : this((ulong)seed, 0x55555555_55555555L, threadSafe) { }
 
 		/// <summary>
 		/// Returns a random floating-point number between 0.0 and 1.0 (exclusive).
@@ -90,33 +90,8 @@ namespace TsMath
 		/// <param name="maxValue">The exclusive upper bound of the random number to be generated.
 		/// It must be greater than or equal to 0.</param>
 		/// <returns>A random number >=0 and &lt; <paramref name="maxValue"/>. 0 if <paramref name="maxValue"/>=0. </returns>
-		public override int Next(int maxValue)
-		{
-			if (maxValue <= 0)
-			{
-				if (maxValue == 0)
-					return 0;
-				throw new ArgumentOutOfRangeException();
-			}
-			return (int)(nextSampleFunc() % (ulong)maxValue);
-		}
-
-		/// <summary>
-		/// Returns a non-negative random integer less than <paramref name="maxValue"/>.
-		/// </summary>
-		/// <param name="maxValue">The exclusive upper bound of the random number to be generated.
-		/// It must be greater than or equal to 0.</param>
-		/// <returns>A random number >=0 and &lt; <paramref name="maxValue"/>. 0 if <paramref name="maxValue"/>=0. </returns>
-		public long Next(long maxValue)
-		{
-			if (maxValue <= 0)
-			{
-				if (maxValue == 0)
-					return 0;
-				throw new ArgumentOutOfRangeException();
-			}
-			return (long)(nextSampleFunc() % (ulong)maxValue);
-		}
+		/// <exception cref="ArgumentOutOfRangeException"><paramref name="maxValue"/> is less than 0.</exception>
+		public override int Next(int maxValue) => this.NextValue(maxValue);
 
 		/// <summary>
 		/// Returns a random integer that is within a specified range.
@@ -125,7 +100,7 @@ namespace TsMath
 		/// <param name="maxValue">The exclusive upper bound of the random number returned.</param>
 		/// <returns>A integer greater than or equal to <paramref name="minValue"/> and less than <paramref name="maxValue"/>.
 		/// </returns>
-		public override int Next(int minValue, int maxValue) => minValue + (int)Next((maxValue - minValue));
+		public override int Next(int minValue, int maxValue) => minValue + this.NextValue(maxValue - minValue);
 
 		/// <summary>
 		/// Fills the elements of a specified array of bytes with random numbers.
@@ -150,4 +125,118 @@ namespace TsMath
 		}
 
 	}
+
+	/// <summary>
+	/// Extension function for random number generation.
+	/// </summary>
+	public static class RandomXorShiftExtensions
+	{
+
+		/// <summary>
+		/// Returns a non-negative random integer less than <paramref name="maxValue"/>.
+		/// </summary>
+		/// <param name="rand">The random number generator to use.</param>
+		/// <param name="maxValue">The exclusive upper bound of the random number to be generated.
+		/// It must be greater than or equal to 0.</param>
+		/// <returns>A random number >=0 and &lt; <paramref name="maxValue"/>. 0 if <paramref name="maxValue"/>=0. </returns>
+		/// <exception cref="ArgumentOutOfRangeException"><paramref name="maxValue"/> is less than 0.</exception>
+		public static int NextValue(this RandomXorShift rand, int maxValue)
+		{
+			if (maxValue <= 0)
+			{
+				if (maxValue == 0)
+					return 0;
+				throw new ArgumentOutOfRangeException();
+			}
+			return (int)(rand.nextSampleFunc() % (ulong)maxValue);
+		}
+
+		/// <summary>
+		/// Returns a non-negative random <see cref="Int64"/> integer less than <paramref name="maxValue"/>.
+		/// </summary>
+		/// <param name="rand">The random number generator to use.</param>
+		/// <param name="maxValue">The exclusive upper bound of the random number to be generated.
+		/// It must be greater than or equal to 0.</param>
+		/// <returns>A random number >=0 and &lt; <paramref name="maxValue"/>. 0 if <paramref name="maxValue"/>=0. </returns>
+		/// <exception cref="ArgumentOutOfRangeException"><paramref name="maxValue"/> is less than 0.</exception>
+		public static long NextValue(this RandomXorShift rand, long maxValue)
+		{
+			if (maxValue <= 0)
+			{
+				if (maxValue == 0)
+					return 0;
+				throw new ArgumentOutOfRangeException();
+			}
+			return (long)(rand.nextSampleFunc() % (ulong)maxValue);
+		}
+
+		/// <summary>
+		/// Returns a random <see cref="Int64"/> integer that is within a specified range.
+		/// </summary>
+		/// <param name="rand">The random number generator to use.</param>
+		/// <param name="minValue">The inclusive lower bound of the random number returned.</param>
+		/// <param name="maxValue">The exclusive upper bound of the random number returned.</param>
+		/// <returns>A integer greater than or equal to <paramref name="minValue"/> and less than <paramref name="maxValue"/>.
+		/// </returns>
+		public static long NextValue(this RandomXorShift rand, long minValue, long maxValue) => minValue + rand.NextValue(maxValue - minValue);
+
+		/// <summary>
+		/// Returns a non-negative random <see cref="BigInteger"/> integer less than <paramref name="maxValue"/>.
+		/// </summary>
+		/// <param name="rand">The random number generator to use.</param>
+		/// <param name="maxValue">The exclusive upper bound of the random number to be generated.
+		/// It must be greater than or equal to 0.</param>
+		/// <returns>A random number >=0 and &lt; <paramref name="maxValue"/>. 0 if <paramref name="maxValue"/>=0. </returns>
+		/// <exception cref="ArgumentOutOfRangeException"><paramref name="maxValue"/> is less than 0.</exception>
+		public static BigInteger NextValue(this RandomXorShift rand, BigInteger maxValue)
+		{
+			if (maxValue.Sign <= 0)
+			{
+				if (maxValue.IsZero)
+					return 0;
+				throw new ArgumentOutOfRangeException();
+			}
+			int ndigits = maxValue.DigitCount;
+			uint[] digits = new uint[ndigits];
+			var rval = rand.nextSampleFunc();
+			int rIndex = 0;
+			for (int i = 0; i < ndigits; i++)
+			{
+				digits[i] = (uint)rval;
+				rIndex++;
+				if (rIndex < 2)
+					rval >>= BigInteger.BaseOps.BitsPerDigit;
+				else
+				{
+					rIndex = 0;
+					rval = rand.nextSampleFunc();
+				}
+			}
+			var num = new BigInteger(digits, false);
+			return num % maxValue;
+		}
+
+		/// <summary>
+		/// Generates a next random number for a random variable of the normal (Gauss) distribution.
+		/// </summary>
+		/// <remarks>
+		/// The method uses the [Box–Muller transform](https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform)
+		/// which is reasonably fast.
+		/// </remarks>
+		/// <param name="rand">The random number generator to use for generating uniformly distributed numbers.</param>
+		/// <param name="mean">The mean of the normal distribution.</param>
+		/// <param name="stdDev">The standard deviation of the normal distribution.</param>
+		/// <returns>A normally distributed random number.</returns>
+		public static double NextGaussian(this Random rand, double mean = 0, double stdDev = 1)
+		{
+			double u1 = 1.0 - rand.NextDouble();
+			double u2 = rand.NextDouble();
+			
+			const double twoPi = 2.0 * Math.PI;
+			double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(twoPi * u2); //random normal(0,1)
+			return mean + stdDev * randStdNormal;
+		}
+
+	}
+
 }
